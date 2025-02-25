bfa3158a0851cc475dfcbc36f0ec3938f8311a6c
znguye <dnguyen.jmblc@gmail.com>
2025-02-25 22:48:28 +0100

    all front end elements

---
+++ javascript/game.js
@@ -0,0 +1,37 @@
+// Grabbing screens
+const gameIntro = document.getElementById("game-intro");
+const gameInstruction = document.getElementById("game-instruction");
+const gameContainer = document.getElementById("game-container");
+const gameEnd = document.getElementById("game-end");
+const winScreen = document.getElementById("win-screen");
+const gameOverScreen = document.getElementById("game-over-screen");
+
+// Grabbing buttons
+const howToLoot = document.getElementById("how-to-loot");
+const startLooting = document.getElementById("start-game");
+const chessboard = document.getElementById("chessboard"); //fake for now
+const playAgain = document.getElementById("play-again");
+
+// Changing screens
+
+howToLoot.addEventListener("click", () =>{
+    gameIntro.style.display = "none";
+    gameInstruction.style.display = "flex";
+})
+
+startLooting.addEventListener("click", () =>{
+    gameInstruction.style.display = "none";
+    gameContainer.style.display = "flex";
+})
+
+chessboard.addEventListener("click", () =>{
+    gameContainer.style.display = "none";
+    gameEnd.style.display = "flex";
+    winScreen.style.display = "flex";
+})
+
+playAgain.addEventListener("click", () =>{
+    gameIntro.style.display = "block";
+    gameEnd.style.display = "none";
+    winScreen.style.display = "none";
+})
